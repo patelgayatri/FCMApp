1bbffc7739b50c5f0d68a54c2dd2783bc6f397a0
patelgayatri <gayatripatel566@gmail.com>
2020-11-25 15:50:30 +1100

    -initial commit

---
+++ app/src/main/java/com/techand/firebaseapp/Constants.kt
@@ -0,0 +1,10 @@
+package com.techand.firebaseapp
+
+class Constants {
+
+    companion object{
+        const val BASE_URL="https://fcm.googleapis.com"
+        const val SERVER_KEY="AAAACc5UXUw:APA91bFJ4x73zAjrI4kDwsws-uOCh1sbD9l-HMv_jPqyqAWRS9_EvtkDoL4K9IM4r2N57qKNIELvC6je31CPehkpC5NdIxnoE39OuBPyrOFCPylWA2flZuI-F8IfHnnE6s8nJZFw_bdL"
+        const val CONTENT_TYPE= "application/json"
+    }
+}
---
+++ app/src/main/java/com/techand/firebaseapp/NotificationData.kt
@@ -0,0 +1,6 @@
+package com.techand.firebaseapp
+
+data class NotificationData(
+    val title: String,
+    val message: String
+)
