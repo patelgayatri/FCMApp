058c6ee5eabf31cfb449b545a6570b7619adab51
Gayatri <gayatripatel566@gmail.com>
2020-11-26 12:03:18 +1100

    Update Constants.kt

--- app/src/main/java/com/techand/firebaseapp/Constants.kt
+++ app/src/main/java/com/techand/firebaseapp/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object{
         const val BASE_URL="https://fcm.googleapis.com"
-        const val SERVER_KEY="AAAACc5UXUw:APA91bFJ4x73zAjrI4kDwsws-uOCh1sbD9l-HMv_jPqyqAWRS9_EvtkDoL4K9IM4r2N57qKNIELvC6je31CPehkpC5NdIxnoE39OuBPyrOFCPylWA2flZuI-F8IfHnnE6s8nJZFw_bdL"
+        const val SERVER_KEY="Add Your Server Key"
         const val CONTENT_TYPE= "application/json"
     }
 }
